bf9d61a7c095c3ecd096ffdd6f9b6cc00cc5c774
Sudersan Raghul <raghulmz@gmail.com>
2024-10-20 15:47:00 +0100

    Remove tabufline maps

--- configsets/from-scratch/lua/mappings.lua
+++ configsets/from-scratch/lua/mappings.lua
@@ -22,20 +22,6 @@ end, { desc = "General Format file" })
 -- global lsp mappings
 map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "LSP Diagnostic loclist" })
 
--- tabufline
--- map("n", "<leader>b", "<cmd>enew<CR>", { desc = "buffer new" })
---
--- map("n", "<tab>", function()
---   require("nvchad.tabufline").next()
--- end, { desc = "buffer goto next" })
---
--- map("n", "<S-tab>", function()
---   require("nvchad.tabufline").prev()
--- end, { desc = "buffer goto prev" })
---
--- map("n", "<leader>x", function()
---   require("nvchad.tabufline").close_buffer()
--- end, { desc = "buffer close" })
 
 -- Comment
 map("n", "<leader>/", "gcc", { desc = "Toggle Comment", remap = true })
