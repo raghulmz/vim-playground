847f0337d16844316c73d156f9cf739245446e9e
Sudersan Raghul <raghulmz@gmail.com>
2024-11-23 21:03:41 +0000

    [conform] disable format on save

--- configsets/from-scratch/lua/plugins/conform.lua
+++ configsets/from-scratch/lua/plugins/conform.lua
@@ -6,10 +6,6 @@ return {
         python = { "isort", "black" },
         -- You can customize some of the format options for the filetype (:help conform.format)
         rust = { "rustfmt", lsp_format = "fallback" },
-        format_on_save = {
-            -- These options will be passed to conform.format()
-            timeout_ms = 500,
-            lsp_format = "fallback",
-        },
+        format_on_save = nil,
     },
 }
