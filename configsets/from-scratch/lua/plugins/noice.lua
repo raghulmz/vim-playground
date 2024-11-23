ed0ab05469dc930aaa2490814eff285b68cc51fa
Sudersan Raghul <raghulmz@gmail.com>
2024-11-23 21:02:59 +0000

    Enable noice

--- configsets/from-scratch/lua/plugins/noice.lua
+++ configsets/from-scratch/lua/plugins/noice.lua
@@ -1,37 +1,34 @@
-return {}
--- nvim in tmux flickers when Noice is enabled
---
--- return {
---     {
---         "folke/noice.nvim",
---         event = "VeryLazy",
---         opts = {
---             -- add any options here
---             presets = {
---                 bottom_search = false, -- use a classic bottom cmdline for search
---                 command_palette = true, -- position the cmdline and popupmenu together
---                 long_message_to_split = true, -- long messages will be sent to a split
---                 inc_rename = false, -- enables an input dialog for inc-rename.nvim
---                 lsp_doc_border = false, -- add a border to hover docs and signature help
---             },
---             messages = {
---                 -- NOTE: If you enable messages, then the cmdline is enabled automatically.
---                 -- This is a current Neovim limitation.
---                 enabled = false,   -- enables the Noice messages UI
---                 view = "notify",   -- default view for messages
---                 view_error = "notify", -- view for errors
---                 view_warn = "notify", -- view for warnings
---                 view_history = "messages", -- view for :messages
---                 view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
---             },
---         },
---         dependencies = {
---             -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
---             "MunifTanjim/nui.nvim",
---             -- OPTIONAL:
---             --   `nvim-notify` is only needed, if you want to use the notification view.
---             --   If not available, we use `mini` as the fallback
---             "rcarriga/nvim-notify",
---         }
---     },
--- }
+return {
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add any options here
+            presets = {
+                bottom_search = false, -- use a classic bottom cmdline for search
+                command_palette = true, -- position the cmdline and popupmenu together
+                long_message_to_split = true, -- long messages will be sent to a split
+                inc_rename = true, -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = true, -- add a border to hover docs and signature help
+            },
+            messages = {
+                -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+                -- This is a current Neovim limitation.
+                enabled = false,   -- enables the Noice messages UI
+                view = "notify",   -- default view for messages
+                view_error = "notify", -- view for errors
+                view_warn = "notify", -- view for warnings
+                view_history = "messages", -- view for :messages
+                view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+            },
+        },
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        }
+    },
+}
